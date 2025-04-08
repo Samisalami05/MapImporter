1bbd759ab091f12f0102f479ec7fd3f902fd352b
samisalami <samgus05@gmail.com>
2025-04-08 20:54:44 +0200

    Updated version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using MelonLoader;
 using MapImporter; // The namespace of your mod class
 // ...
-[assembly: MelonInfo(typeof(MapImporter.Main), "MapImporter", "1.2.0", "Samisalami")]
+[assembly: MelonInfo(typeof(MapImporter.Main), "MapImporter", "1.2.1", "Samisalami")]
 [assembly: MelonGame("Hanki Games", "Sledders")]
 
 // General Information about an assembly is controlled through the following
